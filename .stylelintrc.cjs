f56a740c35a1e1e475a991cf0eb4e654f66cc9e5
ZhangYahan <yahan233@163.com>
2023-08-11 18:15:57 +0800

    feat: vanilla typescript web project cli.

---
+++ .stylelintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-config-html'
+  ],
+  defaultSeverity: 'warning',
+  plugins: ['stylelint-order'],
+  rules: {},
+  overrides: [
+    {
+      files: ['*.less', '**/*.less'],
+      customSyntax: 'postcss-less',
+    }
+  ]
+}
